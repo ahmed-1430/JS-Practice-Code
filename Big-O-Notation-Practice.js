7a13dff064957749afb0df2d8f5d7574703e3c4e
AHMED <ahmedtexting@gmail.com>
2026-04-22 23:29:36 +0600

    add get time of mapping in loop

--- Big-O-Notation-Practice.js
+++ Big-O-Notation-Practice.js
@@ -1,9 +1,29 @@
-const starTime = performance.now()
+// const starTime = performance.now()
 
-for( let i = 1; i < 5000; i++){
-    console.log(i);
+// for( let i = 1; i < 5000; i++){
+//     console.log(i);
+// }
+
+// const endTime = performance.now()
+
+// console.log(`this code took time ${endTime - starTime}ms`);
+
+
+const firstArray = [];
+const secondArray = [];
+for (let i = 1; i <= 60000; i++) {
+    if (i <= 30000) {
+        firstArray.push(i)
+    }
+    secondArray.push(i)
 }
+console.log(`First Array length = ${firstArray.length}`);
+console.log(`Second Array length = ${secondArray.length}`);
 
-const endTime = performance.now()
+console.time("map1")
+const firsUserList = firstArray.map((Number) => ({userId: Number}))
+console.timeEnd("map1")
 
-console.log(`this code took time ${endTime - starTime}ms`);
+console.time("map2")
+const SecondUserList = secondArray.map((Number) => ({userId: Number}))
+console.timeEnd("map2")
