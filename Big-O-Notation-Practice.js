d593c49626e2ce4a2550252c5330d55c153258c6
AHMED <ahmedtexting@gmail.com>
2026-04-22 22:17:10 +0600

    get time of loop

--- Big-O-Notation-Practice.js
+++ Big-O-Notation-Practice.js
@@ -1,3 +1,9 @@
+const starTime = performance.now()
+
 for( let i = 1; i < 5000; i++){
     console.log(i);
 }
+
+const endTime = performance.now()
+
+console.log(`this code took time ${endTime - starTime}ms`);
