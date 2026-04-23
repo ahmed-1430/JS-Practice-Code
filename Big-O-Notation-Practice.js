5c313b5bd311ca92558fc3164f9eab6959644da6
AHMED <ahmedtexting@gmail.com>
2026-04-23 21:14:35 +0600

    array to set => to avoid duplicay

--- Big-O-Notation-Practice.js
+++ Big-O-Notation-Practice.js
@@ -9,21 +9,37 @@
 // console.log(`this code took time ${endTime - starTime}ms`);
 
 
-const firstArray = [];
-const secondArray = [];
-for (let i = 1; i <= 60000; i++) {
-    if (i <= 30000) {
-        firstArray.push(i)
-    }
-    secondArray.push(i)
-}
-console.log(`First Array length = ${firstArray.length}`);
-console.log(`Second Array length = ${secondArray.length}`);
+// const firstArray = [];
+// const secondArray = [];
+// for (let i = 1; i <= 60000; i++) {
+//     if (i <= 30000) {
+//         firstArray.push(i)
+//     }
+//     secondArray.push(i)
+// }
+// console.log(`First Array length = ${firstArray.length}`);
+// console.log(`Second Array length = ${secondArray.length}`);
 
-console.time("map1")
-const firsUserList = firstArray.map((Number) => ({userId: Number}))
-console.timeEnd("map1")
+// console.time("map1")
+// const firsUserList = firstArray.map((Number) => ({userId: Number}))
+// console.timeEnd("map1")
 
-console.time("map2")
-const SecondUserList = secondArray.map((Number) => ({userId: Number}))
-console.timeEnd("map2")
+// console.time("map2")
+// const SecondUserList = secondArray.map((Number) => ({userId: Number}))
+// console.timeEnd("map2")
+
+// console.time("find")
+// const user = secondArray.find((user) => user.userId === 500000)
+// console.timeEnd("find")
+
+
+const set = new Set();
+set.add("apple");
+set.add("banana");
+
+console.log(set);
+
+const array = ["apple", "Banana", "Mango", "berry", "apple"]
+
+const newSet = new Set(array)
+console.log(newSet);
