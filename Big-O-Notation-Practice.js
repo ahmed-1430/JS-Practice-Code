155e5e017d8dbe9352e0619b1f0ca9aa744e67a9
AHMED <ahmedtexting@gmail.com>
2026-04-23 21:47:28 +0600

    convert set to an array

--- Big-O-Notation-Practice.js
+++ Big-O-Notation-Practice.js
@@ -1,36 +1,36 @@
-// const starTime = performance.now()
+const starTime = performance.now()
 
-// for( let i = 1; i < 5000; i++){
-//     console.log(i);
-// }
+for( let i = 1; i < 5000; i++){
+    console.log(i);
+}
 
-// const endTime = performance.now()
+const endTime = performance.now()
 
-// console.log(`this code took time ${endTime - starTime}ms`);
+console.log(`this code took time ${endTime - starTime}ms`);
 
 
-// const firstArray = [];
-// const secondArray = [];
-// for (let i = 1; i <= 60000; i++) {
-//     if (i <= 30000) {
-//         firstArray.push(i)
-//     }
-//     secondArray.push(i)
-// }
-// console.log(`First Array length = ${firstArray.length}`);
-// console.log(`Second Array length = ${secondArray.length}`);
+const firstArray = [];
+const secondArray = [];
+for (let i = 1; i <= 60000; i++) {
+    if (i <= 30000) {
+        firstArray.push(i)
+    }
+    secondArray.push(i)
+}
+console.log(`First Array length = ${firstArray.length}`);
+console.log(`Second Array length = ${secondArray.length}`);
 
-// console.time("map1")
-// const firsUserList = firstArray.map((Number) => ({userId: Number}))
-// console.timeEnd("map1")
+console.time("map1")
+const firsUserList = firstArray.map((Number) => ({userId: Number}))
+console.timeEnd("map1")
 
-// console.time("map2")
-// const SecondUserList = secondArray.map((Number) => ({userId: Number}))
-// console.timeEnd("map2")
+console.time("map2")
+const SecondUserList = secondArray.map((Number) => ({userId: Number}))
+console.timeEnd("map2")
 
-// console.time("find")
-// const user = secondArray.find((user) => user.userId === 500000)
-// console.timeEnd("find")
+console.time("find")
+const user = secondArray.find((user) => user.userId === 500000)
+console.timeEnd("find")
 
 
 const set = new Set();
@@ -43,3 +43,9 @@ const array = ["apple", "Banana", "Mango", "berry", "apple"]
 
 const newSet = new Set(array)
 console.log(newSet);
+
+// set convert to an Array
+const test  = Array.from(newSet)
+test.push("tomato")
+
+console.log(test);
